6cd1ca33979657d578d29ac8e0373f9b0ff0c0f3
Rhuz-Namzug <20255023@s.ubaguio.edu>
2026-02-09 12:40:18 +0800

    created objects of products

--- src/App.jsx
+++ src/App.jsx
@@ -13,4 +13,94 @@ let  App = () => {
   )
 }
 
+const products = [
+  {
+    id: 1,
+    name: "Dumbell",
+    category: "0-15 kg",
+    price:  249.99-499.99,
+    inStock: true
+  },
+  
+  {
+    id: 2,
+    name: "Dumbell",
+    category: "16 - 25 kg",
+    price:  599.99,
+    inStock: true
+  },
+
+  {
+    id: 3,
+    name: "Dumbell",
+    category: "26-40 kg",
+    price:  899.99,
+    inStock: true
+  },
+
+  {
+    id: 4,
+    name: "Dumbell",
+    category: "41-50 kg",
+    price:  1199.99,
+    inStock: true
+  },
+
+  {
+    id: 5,
+    name: "Barbell",
+    category: "20 kg",
+    price:  499.99,
+    inStock: true
+  },
+
+  {
+    id: 6,
+    name: "Lifting Straps",
+    category: "Accessories",
+    price:  249.99,
+    inStock: true
+  },
+
+  {
+    id: 7,
+    name: "Wrist Wraps",
+    category: "Accessories",
+    price:  249.99,
+    inStock: true
+  },
+
+  {
+    id: 7,
+    name: "Wrist Bands",
+    category: "Accessories",
+    price:  249.99,
+    inStock: false
+  },
+
+  {
+    id: 8,
+    name: "Lifting Belt",
+    category: "Accessories",
+    price:  1499.99,
+    inStock: true
+  },
+
+  {
+    id: 9,
+    name: "Resistance Bands",
+    category: "Accessories",
+    price:  149.99-599.99,
+    inStock: false
+  },
+
+  {
+    id: 10,
+    name: "Chalk",
+    category: "Accessories",
+    price:  149.99,
+    inStock: true
+  }
+];
+
 export default App
